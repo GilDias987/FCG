e500e0677144eefcf5ded74d811443f523f39064
DESKTOP-0SJTD8R\Alexandre Silva <horizonprog@gmail.com>
2025-10-27 12:31:51 -0300

    Grupo de usuário - Manter/ObterTodos

--- FCG/FCG.Application.UseCases/Feature/Usuario/Commands/AddGrupoUsuario/AddGrupoUsuarioCommand.cs
+++ FCG/FCG.Application.UseCases/Feature/Usuario/Commands/AddGrupoUsuario/AddGrupoUsuarioCommand.cs
@@ -1,8 +1,11 @@
 ﻿using MediatR;
 
+// Dependências
+using FCG.ApplicationCore.Dto.Usuario;
+
 namespace FCG.Application.UseCases.Feature.Usuario.Commands.AddGrupoUsuario
 {
-    public class AddGrupoUsuarioCommand : IRequest<int>
+    public class AddGrupoUsuarioCommand : IRequest<GrupoUsuarioDto>
     {
         public required string Nome { get; set; } 
     }
